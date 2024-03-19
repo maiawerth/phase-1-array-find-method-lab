32e34e1a059b7a8008a519e39ffc71de8e5a33d8
Maia Werth <maia.moo@me.com>
2024-03-18 19:07:53 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const winObject = record.find(obj => obj.result === "W");
+  
+    if (winObject) {
+      return winObject.year;
+    } else {
+      return undefined;
+    }
+  }
